e2d4521b5576ca21778ab13b073609bc2732dd08
Omar <devomarhamdi@gmail.com>
2023-11-20 14:15:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/users', userRouter);
+
+module.exports = app;
