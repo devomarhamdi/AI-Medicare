8d2480e2d46373bc50948f795b06fdf158eb7a7e
Omar <devomarhamdi@gmail.com>
2023-11-20 15:48:16 +0200

    added user model and deleted eslint

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const validator = require('validator');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
