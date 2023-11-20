87d5335bfd5a40e97da300dcdf8fa55e2938f949
Omar <devomarhamdi@gmail.com>
2023-11-20 18:44:28 +0200

    fixing mongo error

--- server.js
+++ server.js
@@ -12,13 +12,7 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE;
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('DB connection successful!'));
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
