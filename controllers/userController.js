b1a5d4f8509e3b2c31fb87d53f92c5a1f4d470cf
Omar <devomarhamdi@gmail.com>
2023-11-30 20:08:51 +0200

    added update me

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,42 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+exports.updateMe = async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filter unwanted fields
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 2) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
