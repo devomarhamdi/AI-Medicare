eadfadf550708a8b29b183f5cca7fa5290c2c505
Omar <devomarhamdi@gmail.com>
2023-12-10 15:20:51 +0200

    BMI done successfully

--- controllers/Patient/healthController.js
+++ controllers/Patient/healthController.js
@@ -7,9 +7,20 @@ exports.BMI = (req, res, next) => {
   if (!Height || !Weight) {
     return next(new AppError('Please enter your weight or height', 404));
   }
-  const bmi = (Weight / (Height * Height)) * 10000;
+  const bmi = ((Weight / (Height * Height)) * 10000).toFixed(1);
+  let weightStatus = null;
+  if (bmi >= 30) {
+    weightStatus = 'Obesity';
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    weightStatus = 'Overweight';
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    weightStatus = 'Healthy Weight';
+  } else {
+    weightStatus = 'Underweight';
+  }
   res.status(200).json({
     status: 'success',
-    bmi
+    bmi,
+    weightStatus
   });
 };
