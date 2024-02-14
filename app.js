de114ad7c4fa131fbcd7fc3e552a3823f2eed659
Omar <devomarhamdi@gmail.com>
2024-02-14 23:54:41 +0200

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+app.use(cors());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
